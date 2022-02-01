514aeba433aa293db50c8596114c2f07d94fec02
Salih Güngörmez <salihgungormez@outlook.com>
2022-02-01 09:59:01 +0300

    Update ThemeButton.js

--- src/markup/Element/ThemeButton/ThemeButton.js
+++ src/markup/Element/ThemeButton/ThemeButton.js
@@ -1,12 +1,17 @@
-import React,{Component} from 'react';			
+import React, { Component } from "react";
 
-export default class ThemeButton extends Component{
-	render(){
-		return(
-			<>
-				<a href="https://1.envato.market/3zZ9y" className="bt-buy-now theme-btn"><i className="ti-shopping-cart"></i><span>Buy Now</span></a>
-				<a href="https://w3itexperts.ticksy.com"  className="bt-support-now theme-btn"><i className="ti-headphone-alt"></i><span>Support</span></a>
-			</>
-		)
-	}	
+export default class ThemeButton extends Component {
+  render() {
+    return (
+      <>
+        <a
+          href="https://api.whatsapp.com/send?phone=+90(532)667 94 63&text=Merhaba"
+          class="float"
+          target="_blank"
+        >
+          <i class="fa fa-whatsapp my-float"></i>
+        </a>
+      </>
+    );
+  }
 }
