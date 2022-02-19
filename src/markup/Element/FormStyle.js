eaba129ede936c938dc57d76382bca60dd46d33d
psychep <salihgungormez@outlook.com>
2022-02-19 16:18:10 +0300

    Update FormStyle.js

--- src/markup/Element/FormStyle.js
+++ src/markup/Element/FormStyle.js
@@ -1,154 +1,156 @@
-import React, { Fragment, Component } from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
-class FormStyle extends Component {
-  render() {
-    return (
-      <Fragment>
-        <div className="container">
+const FormStyle = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_8kjnr3o",
+        "template_q16r2im",
+        form.current,
+        "user_ijt9nhCteZejMvvrDlvuZ"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <>
+      <div className="container">
+        <div className="">
           <div className="">
-            <div className="">
-              <form
-                className="inquiry-form  dzForm"
-                action="script/contact.php"
-              >
-                <div className="dzFormMsg"></div>
-                <h3 className="box-title m-t0 m-b10">
-                  Teklif Formunu doldurun <span className="bg-primary"></span>
-                </h3>
-                <p>İhtiyaçlarınıza yönelik en uygun teklifi sizlere sunalım</p>
-                <div className="row">
-                  <div className="col-lg-6">
-                    <div className="form-group">
-                      <div className="input-group">
-                        <span className="input-group-addon">
-                          <i className="ti-user text-primary"></i>
-                        </span>
-                        <input
-                          name="dzName"
-                          type="text"
-                          required
-                          className="form-control"
-                          placeholder="Ad-Soyad"
-                        />
-                      </div>
+            <form
+              ref={form}
+              onSubmit={sendEmail}
+              className="inquiry-form  dzForm"
+            >
+              <div className="dzFormMsg"></div>
+              <h3 className="box-title m-t0 m-b10">
+                Teklif Formunu doldurun <span className="bg-primary"></span>
+              </h3>
+              <p>İhtiyaçlarınıza yönelik en uygun teklifi sizlere sunalım</p>
+              <div className="row">
+                <div className="col-lg-6">
+                  <div className="form-group">
+                    <div className="input-group">
+                      <span className="input-group-addon">
+                        <i className="ti-user text-primary"></i>
+                      </span>
+                      <input
+                        type="text"
+                        name="user_name"
+                        required
+                        className="form-control"
+                        placeholder="Ad-Soyad"
+                      />
                     </div>
-                  </div>
-                  <div className="col-lg-6">
-                    <div className="form-group">
-                      <div className="input-group">
-                        <span className="input-group-addon">
-                          <i className="ti-mobile text-primary"></i>
-                        </span>
-                        <input
-                          name="dzOther[Phone]"
-                          type="text"
-                          required
-                          className="form-control"
-                          placeholder="Telefon"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-12">
-                    <div className="form-group">
-                      <div className="input-group">
-                        <span className="input-group-addon">
-                          <i className="ti-email text-primary"></i>
-                        </span>
-                        <input
-                          name="dzEmail"
-                          type="email"
-                          className="form-control"
-                          required
-                          placeholder="E-mail"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-6">
-                    <div className="form-group">
-                      <div className="input-group">
-                        <span className="input-group-addon">
-                          <i className="ti-check-box text-primary"></i>
-                        </span>
-                        <input
-                          name="dzOther[Subject]"
-                          type="text"
-                          required
-                          className="form-control"
-                          defaultValue={this.props.category}
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-6">
-                    <div className="form-group">
-                      <div className="input-group">
-                        <span className="input-group-addon">
-                          <i className="ti-file text-primary"></i>
-                        </span>
-                        <input
-                          name="dzOther[Subject]"
-                          type="text"
-                          required
-                          className="form-control"
-                          defaultValue={this.props.produtc}
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-12">
-                    <div className="form-group">
-                      <div className="input-group">
-                        <span className="input-group-addon">
-                          <i className="ti-agenda text-primary"></i>
-                        </span>
-                        <textarea
-                          name="dzMessage"
-                          rows="4"
-                          className="form-control"
-                          required
-                          placeholder="Mesajınnız"
-                        ></textarea>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-12">
-                    <div className="form-group">
-                      <div className="input-group">
-                        <div
-                          className="g-recaptcha"
-                          data-sitekey="6LefsVUUAAAAADBPsLZzsNnETChealv6PYGzv3ZN"
-                          data-callback="verifyRecaptchaCallback"
-                          data-expired-callback="expiredRecaptchaCallback"
-                        ></div>
-                        <input
-                          className="form-control d-none"
-                          data-recaptcha="true"
-                          required
-                          data-error="Please complete the Captcha"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-12">
-                    <button
-                      name="submit"
-                      type="submit"
-                      value="Submit"
-                      className="site-button button-lg"
-                    >
-                      {" "}
-                      <span>Teklif alın!</span>{" "}
-                    </button>
                   </div>
                 </div>
-              </form>
-            </div>
+                <div className="col-lg-6">
+                  <div className="form-group">
+                    <div className="input-group">
+                      <span className="input-group-addon">
+                        <i className="ti-mobile text-primary"></i>
+                      </span>
+                      <input
+                        name="message"
+                        type="text"
+                        required
+                        className="form-control"
+                        placeholder="Telefon"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-12">
+                  <div className="form-group">
+                    <div className="input-group">
+                      <span className="input-group-addon">
+                        <i className="ti-email text-primary"></i>
+                      </span>
+                      <input
+                        type="email"
+                        name="user_email"
+                        className="form-control"
+                        required
+                        placeholder="E-mail"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6">
+                  <div className="form-group">
+                    <div className="input-group">
+                      <span className="input-group-addon">
+                        <i className="ti-check-box text-primary"></i>
+                      </span>
+                      <input
+                        name="message"
+                        type="text"
+                        required
+                        className="form-control"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6">
+                  <div className="form-group">
+                    <div className="input-group">
+                      <span className="input-group-addon">
+                        <i className="ti-file text-primary"></i>
+                      </span>
+                      <input
+                        name="message"
+                        type="text"
+                        required
+                        className="form-control"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-12">
+                  <div className="form-group">
+                    <div className="input-group">
+                      <span className="input-group-addon">
+                        <i className="ti-agenda text-primary"></i>
+                      </span>
+                      <textarea
+                        name="message"
+                        rows="4"
+                        className="form-control"
+                        required
+                        placeholder="Mesajınnız"
+                      ></textarea>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-12"></div>
+                <div className="col-lg-12">
+                  <button
+                    type="submit"
+                    value="Send"
+                    className="site-button button-lg"
+                  >
+                    {" "}
+                    <span>Teklif alın!</span>{" "}
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
-      </Fragment>
-    );
-  }
-}
+      </div>
+    </>
+  );
+};
 export default FormStyle;
